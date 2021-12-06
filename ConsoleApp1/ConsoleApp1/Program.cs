02dac7e787c95e737fc41f0a92422eaf17705de1
ntroyes <n.troyes@gmail.com>
2021-12-06 11:18:07 +0100

    changes

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -12,120 +12,64 @@ namespace ConsoleApp1
         {
         }
         interface IDrivable
-
         {
-
             void Drive(long miles);
-
-            void LaunchRocket();
-
         }
-
+        interface IRocketDrivable :IDrivable
+        {
+            void LaunchRocket();
+        }
 
         class Car : IDrivable
-
         {
-
             public void Drive(long miles)
-
             {
-
                 Console.WriteLine("I am driven to {0} miles away ...", miles);
-
             }
-
-
-            public void LaunchRocket()
-
-            {
-
-                Console.WriteLine("I don't have any rocket to launch ...");
-
-            }
-
         }
 
-
-        class FlyingCar : IDrivable
-
+        class FlyingCar : IRocketDrivable
         {
-
             private bool _isRocketLaunched;
-
-
             public void Drive(long miles)
-
             {
-
                 if (!_isRocketLaunched)
-
                 {
-
                     Console.WriteLine("Launch the rocket first or this flying car can not get driven.");
-
                 }
 
                 else
-
                 {
-
                     Console.WriteLine("I am flying to {0} miles away ...", miles);
-
                 }
-
             }
-
-
             public void LaunchRocket()
-
             {
-
                 _isRocketLaunched = true;
-
             }
-
         }
 
-
-        class SpaceShip : IDrivable
-
+        class SpaceShip : IRocketDrivable
         {
-
             private bool _isRocketLaunched;
-
-
             public void Drive(long miles)
-
             {
-
                 if (!_isRocketLaunched)
-
                 {
-
                     Console.WriteLine("Spaceship can't leave Earth if its rocket is not launched !");
-
                 }
 
                 else
-
                 {
-
                     Console.WriteLine("The spaceship is going {0} miles away", miles);
-
                 }
 
             }
-
-
             public void LaunchRocket()
-
             {
-
                 _isRocketLaunched = true;
 
             }
-
         }
-
     }
 }
